a6b1943bbadab20dd2cecf92d2e433513cd6f777
Artur Signell <artur@vaadin.com>
2019-08-29 14:06:25 +0300

    Place markers on svg to support floorplan resizing

--- frontend/floorplan-view.js
+++ frontend/floorplan-view.js
@@ -1,62 +1,69 @@
-import { html, svg, css, LitElement } from 'lit-element';
-import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
-import { styleMap } from 'lit-html/directives/style-map';
+import { svg, css, LitElement } from "lit-element";
+import { unsafeHTML } from "lit-html/directives/unsafe-html.js";
 
 import floorplanSvg from "!!raw-loader!./assets/floorplan.svg";
-import markerSvg from "!!raw-loader!./assets/floorplan-marker.svg";
+
 import { repeat } from "lit-html/directives/repeat";
-import { sensorData } from './dummy-data'
+import { sensorData } from "./dummy-data";
 
 class FloorplanView extends LitElement {
-    static get styles() {
-        return css`
-        :host {
-            display: block;
-            position: relative;
-        }
-        .floorplan {
-            width: 100%;
-            height: auto;
-        }
-        .marker {
-            width: 40px;
-            height: 40px;
-            position: absolute;
-            top:0;left:0;
-        }
-        `;
-    }
-    render() {
-        return svg`
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+        position: relative;
+        width: 100%;
+      }
+      .floorplan {
+        width: 100%;
+        height: auto;
+      }
+      .marker {
+        width: 40px;
+        height: 40px;
+        position: absolute;
+        top: 0;
+        left: 0;
+      }
+    `;
+  }
+  render() {
+    return svg`
+        <svg class="floorplan" viewbox="0 0 100 100">
         ${unsafeHTML(floorplanSvg)}
-        ${repeat(sensorData, sensor => sensor.id, sensor => html`
-            <svg class="marker" style="${styleMap({ left: sensor.floorplan.x, top: sensor.floorplan.y })}" @click="${e => this.highlight(sensor.floorplan.room)}">
-            ${unsafeHTML(markerSvg)}
-            </svg>
-        `)}
-
+        ${repeat(
+          this.sensors,
+          sensor => sensor.id,
+          sensor => svg`
+          <image x="${sensor.floorplan.x}" y="${sensor.floorplan.y}" width="5" height="5"
+         href="floorplan-marker.svg"></image>
+          `
+        )}
+        </svg>
         `;
-    }
-    highlight(id) {
-
-    }
-    firstUpdated() {
-        super.firstUpdated();
-    }
-    updated() {
-        debugger;
-        super.updated();
-        sensorData.forEach(sensor => {
-            if (sensor.floorplan.room) {
-                var style = this.shadowRoot.querySelector("#" + sensor.floorplan.room).style;
-                const alpha = Math.min((Math.abs(sensor.target - sensor.value) / 10), 1.0);
-                if (sensor.target < sensor.value) {
-                    style.fill = `rgba(255,0,0,${alpha})`;
-                } else {
-                    style.fill = `rgba(0,0,255,${alpha})`;
-                }
-            }
-        });
-    }
+  }
+  constructor() {
+    super();
+    this.sensors = sensorData;
+  }
+  updated() {
+    super.updated();
+    this.sensors.forEach(sensor => {
+      if (sensor.floorplan.room) {
+        var style = this.shadowRoot.querySelector("#" + sensor.floorplan.room)
+          .style;
+        const alpha = Math.min(
+          Math.abs(sensor.target - sensor.value) / 10,
+          1.0
+        );
+        style.transition = "fill 1s";
+        if (sensor.target < sensor.value) {
+          style.fill = `rgba(255,0,0,${alpha})`;
+        } else {
+          style.fill = `rgba(0,0,255,${alpha})`;
+        }
+      }
+    });
+  }
 }
 customElements.define("floorplan-view", FloorplanView);
